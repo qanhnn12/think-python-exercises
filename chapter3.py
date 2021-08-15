3fda3bc957ee36f0657d2a7de0d973e0b358eb49
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2021-08-15 17:06:43 +0700

    Update chapter3.py

--- chapter3.py
+++ chapter3.py
@@ -1,4 +1,7 @@
 # Exercise 3.1. 
+''' Write a function named right_justify that takes a string named s as a parameter 
+and prints the string with enough leading spaces so that the last letter of the string is in column 70
+of the display.'''
 
 def right_justify(s):
     print(' '*(70 - len(s)) + s)
