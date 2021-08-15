0d604191b9dfe6de749d5fd6e48bc927e5b60c4c
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2021-08-15 17:05:38 +0700

    Update chapter3.py

--- chapter3.py
+++ chapter3.py
@@ -49,33 +49,6 @@ def grid():
 
 grid()
 
-# 2. Write a function that draws a similar grid with four rows and four columns
-
-def do_twice(f):
-    f()
-    f()
-
-def do_four(f):
-    do_twice(f)
-    do_twice(f)
-
-def border_row():
-    print("+", "-" * 4, "+", "-" * 4, "+", "-" * 4, "+")
-
-def border_col():
-    print("|", " " * 4, "|", " " * 4, "|", " " * 4, "|")
-
-def border_grid():
-    border_row()
-    do_four(border_col)
-
-def grid_four():
-    do_twice(border_grid)
-    border_row()
-    do_four(border_col)
-    border_row()
-    
-grid_four()
 
 '''
 2. Write a function that draws a similar grid with four rows and four columns.
