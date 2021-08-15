1828793563caa362be7c703da43bdd7addb8b781
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2021-08-15 17:35:36 +0700

    Update chapter3.py

--- chapter3.py
+++ chapter3.py
@@ -7,11 +7,57 @@ def right_justify(s):
 
 right_justify('monty')
 
-# Exercise 3.2
 
 
+# Exercise 3.2.
 
-# Exercise 3.3
+'''1. Type this example into a script and test it.'''
+
+def do_twice(f):
+    f()
+    f()
+
+def print_spam():
+    print('spam')
+
+do_twice(print_spam)
+
+
+'''2. Modify do_twice so that it takes two arguments, a function object and a value, and calls the function twice, passing the value as an argument.'''
+
+def do_twice(f, a):
+   f(a)
+   f(a)
+
+def print_spam(word):
+   print(word)
+
+do_twice(print_spam, 'ss')
+
+
+'''3. Copy the definition of print_twice from earlier in this chapter to your script.'''
+
+def print_twice(bruce):
+    print(bruce)
+    print(bruce)
+
+    
+'''4. Use the modified version of do_twice to call print_twice twice, passing 'spam' as an argument.'''
+
+do_twice(print_twice, 'spam')
+
+
+'''5. Define a new function called do_four that takes a function object and a value and calls the function four times, passing the value as a parameter. 
+There should be only two statements in the body of this function, not four.'''
+
+def do_four(print_spam, arg):
+    do_twice(print_spam, arg)
+    do_twice(print_spam, arg)
+
+do_four(print_twice, 'abc')
+        
+        
+# Exercise 3.3.
 '''
 1. Write a function that draws a grid like the following:
 + - - - - + - - - - +
