4a2c4d6623298cc1be7e0a2e42c9a2dcdcfbe2f0
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2021-08-16 09:02:51 +0700

    Update chapter4.py

--- chapter4.py
+++ chapter4.py
@@ -26,7 +26,7 @@ bob = turtle.Turtle()
 square(bob, 250)
 
 ## Exercise 4.3.3.
-# Draw a n-sided regular polygon polygon by adding a parameter named n..
+# Write a n-sided regular polygon by adding a parameter named n.
 # Hint: The exterior angles of an n-sided regular polygon are 360/n degrees.
 
 import turtle
@@ -41,8 +41,8 @@ bob = turtle.Turtle()
 polygon(bob, 6, 80)
 
 ## Exercise 4.3.4.
-# Draw circle, which takes a radius, r, as a parameter.
-# Hint: circumference = length * n
+# Write a circle, which takes a radius named r as a parameter.
+# Hint: circumference = length * n (n is constant).
 
 import turtle
 import math
@@ -62,4 +62,51 @@ def circle(t, r):
 bob = turtle.Turtle()
 circle(bob, 90)
 
+# Rewrite the circle function so n is not constant value.
 
+import turtle
+import math
+
+def polygon(t, n, length):
+    angle = 360 / n
+    for i in range(n):
+        t.fd(length)
+        t.lt(angle)
+
+def circle(t, r):
+    circumference = 2 * math.pi * r
+    n = int(circumference / 3) + 3          # Adding 3 to n guarantees that the polygon has at least 3 sides.
+    length = circumference / n
+    polygon(t, n, length)
+
+bob = turtle.Turtle()
+circle(bob, 75)
+
+## Exercise 4.3.5.
+
+import turtle
+import math
+
+# copy of polygon and transform it into arc    
+def arc(t, r, angle):
+    arc_length = 2 * math.pi * r * angle / 360
+    n = int(arc_length / 3) + 1
+    step_length = arc_length / n
+    step_angle = float(angle) / n
+    polyline(t, n, step_length, step_angle)
+    
+# rewrite polygon and arc to use polyline
+def polyline(t, n, length, angle):
+    for i in range(n):
+        t.fd(length)
+        t.lt(angle)
+
+def polygon(t, n, length):
+    angle = 360.0 / n
+    polyline(t, n, length, angle)
+  
+def circle(t, r):
+    arc(t, r, 360)
+
+bob = turtle.Turtle()
+arc(bob, 100, 180)
