71096e8010ccda5ff0acc2cefa13a058c6787ffa
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2021-08-16 11:55:45 +0700

    Update chapter5.py

--- chapter5.py
+++ chapter5.py
@@ -1,16 +1,18 @@
 ## Exercise 5.1 - Converts it to a time of day in hours, minutes, and seconds, plus the number of days since the epoch.
 
 import time
+total_secs = time.time()
 
-epoch = int(time.time())
+# seconds = the remainder of total seconds divided by the number of seconds in a minute 
+# minutes = the remainder of total minutes divided by the number of minutes in an hour
+# the same applies for hours and days
 
-# calculate number of days since epoch
-days = epoch / (60 * 60 * 24)
-hour = days % int(days) * 24
-min  = hour % int(hour) * 60
-sec  = min % int(min) * 60
+seconds = int(total_secs % 60)
+minutes = int((total_secs // 60) % 60)
+hours = int((total_secs // 3600) % 24)
+days = int(total_secs // (3600 * 24))
 
-print(f"Days since epoch: {int(days)}\nCurrent Time: {int(hour)}:{int(min)}:{int(sec)}")
+print("There have been " + str(days) +" days, " + str(hours) + " hours, " + str(minutes) + " minutes, and " + str(seconds) + " seconds since epoch.")
 
 
 ## Exercise 5.2.
