b41d7a16672da8f3a8367baca7e0a0b0a13cca37
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2021-08-16 12:04:50 +0700

    Update chapter5.py

--- chapter5.py
+++ chapter5.py
@@ -15,4 +15,20 @@ days = int(total_secs // (3600 * 24))
 print("There have been " + str(days) +" days, " + str(hours) + " hours, " + str(minutes) + " minutes, and " + str(seconds) + " seconds since epoch.")
 
 
-## Exercise 5.2.
+## Exercise 5.2 - Check the Fermat’s Last Theorem
+
+def check_format(a, b, c, n):
+    if n > 2 and a**n + b**n == c**n:
+        print("Holy smokes, Fermat was wrong!")
+    else:
+        print("No, that doesn't work.")
+
+def input_num():
+    a = int(input("Choose a number for a: "))
+    b = int(input("Choose a number for b: "))
+    c = int(input("Choose c number for c: "))
+    n = int(input("Choose a number for n: "))
+    return check_format(a, b, c, n)
+
+input_num()
+
