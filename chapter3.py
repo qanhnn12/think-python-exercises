8044c9799af319f1c49e2a01927cdefc49bb63ad
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2021-08-16 07:44:13 +0700

    Update chapter3.py

--- chapter3.py
+++ chapter3.py
@@ -1,7 +1,6 @@
 ### Exercise 3.1. 
 # Write a function named right_justify that takes a string named s as a parameter 
 # and prints the string with enough leading spaces so that the last letter of the string is in column 70 of the display.
-
 def right_justify(s):
     print(' '*(70 - len(s)) + s)
 
@@ -10,7 +9,6 @@ right_justify('monty')
 
 ### Exercise 3.2.
 # 1. Type this example into a script and test it.
-
 def do_twice(f):
     f()
     f()
@@ -23,7 +21,6 @@ do_twice(print_spam)
 
 # 2. Modify do_twice so that it takes two arguments, a function object and a value,
 # and calls the function twice, passing the value as an argument.
-
 def do_twice(f, a):
    f(a)
    f(a)
@@ -35,20 +32,17 @@ do_twice(print_spam, 'ss')
 
 
 # 3. Copy the definition of print_twice from earlier in this chapter to your script.
-
 def print_twice(bruce):
     print(bruce)
     print(bruce)
 
     
 # 4. Use the modified version of do_twice to call print_twice twice, passing 'spam' as an argument.
-
 do_twice(print_twice, 'spam')
 
 
 # 5. Define a new function called do_four that takes a function object and a value and calls the function four times, passing the value as a parameter. 
 # There should be only two statements in the body of this function, not four.'''
-
 def do_four(print_spam, arg):
     do_twice(print_spam, arg)
     do_twice(print_spam, arg)
@@ -71,7 +65,6 @@ do_four(print_twice, 'abc')
 |         |         |
 + - - - - + - - - - +
 '''
-
 def do_twice(f):
     f()
     f()
@@ -116,7 +109,6 @@ grid()
 |      |      |      |
 + ---- + ---- + ---- +
 '''
-
 def do_twice(f):
     f()
     f()
