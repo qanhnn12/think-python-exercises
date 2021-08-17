931c2475e1b1c6a61d4517c8bc37aabc71785b37
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2021-08-17 10:38:01 +0700

    Update chapter6.py

--- chapter6.py
+++ chapter6.py
@@ -44,3 +44,36 @@ Program prints:
 
 # Exercise 6.2 - Write a function named 'ack' that evaluates the Ackermann function
 
+def A(m,n):
+    if m == 0:
+        return n+1
+    elif m > 0 and n == 0:
+        return A(m-1, 1)
+    else:
+        return A(m-1, A(m, n-1))
+
+# Exercise 6.3
+# return the first character of a string
+def first(word):
+    return word[0]
+
+# return the last character of a string
+def last(word):
+    return word[-1]
+
+# return the middle characters of a string, except the first and last ones
+def middle(word):
+    return word[1:-1]
+
+# is_palindrome function that returns True if it is a palindrome and False otherwise
+def is_palindrome(word):
+    if len(word) > 1 and first(word) == last(word):
+        return True
+    else:
+        return False
+    return is_palindrome(middle(word))
+
+print(is_palindrome('mom'))
+print(is_palindrome('bobby'))
+print(is_palindrome('noon'))
+print(is_palindrome('danger'))
