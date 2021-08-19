342c05a89705e77101ebf7db8f4d6e5a12905218
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2021-08-19 09:20:43 +0700

    Update chapter8.py

--- chapter8.py
+++ chapter8.py
@@ -3,7 +3,6 @@ word = 'banana'
 print(word.count('a'))
 
 # EXERCISE 8.3
-
 def is_palindrome(word):
     if len(word) > 1 and word == word[::-1]:
         return True
@@ -12,3 +11,4 @@ def is_palindrome(word):
         
 print(is_palindrome('iloveu'))
 print(is_palindrome('noon'))
+
