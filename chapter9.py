60697ac06d04cc8fef16dc99cb0b16ec5e376aa6
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2021-08-20 10:23:22 +0700

    Update chapter9.py

--- chapter9.py
+++ chapter9.py
@@ -6,5 +6,32 @@ for line in fin:
     if len(word) > 20:
         print(word)
 
-EXERCISE 9.2
+# Write a function called has_no_e that returns True if the given word doesn’t have the letter “e” in it.
+def has_no_e(word):
+    for i in word:
+        if i == 'e':
+            return False
+    return True
+
+print(has_no_e('aespa'))
+
+# Write a program that reads words.txt and prints only the words that have no “e”
+# Compute the percentage of words in the list that have no “e”.
+fin = open("words.txt")
+total = 0
+no_e_count = 0
+
+for line in fin:
+    word = line.strip()
+    if 'e' not in word:                         # or we could use the has_no_e function above: if has_no_e(word)
+        no_e_count += 1
+        print(word)
+    total += 1
+
+no_e_pct = no_e_count / total * 100
+print(no_e_pct, 'percentage of words in the list that have no “e”.')
+
+
+# EXERCISE 9.2
+
 
