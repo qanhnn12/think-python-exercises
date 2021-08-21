19e2f56ea2634eea3ec7c6a3d1ba7263b5c1d80e
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2021-08-21 09:16:47 +0700

    Update chapter9.py

--- chapter9.py
+++ chapter9.py
@@ -1,5 +1,6 @@
 # EXERCISE 9.1. 
 # Write a program that reads words.txt, prints words with more than 20 Characters (not counting whitespace).
+
 fin = open('words.txt')
 for line in fin:
     word = line.strip()
@@ -9,6 +10,7 @@ for line in fin:
         
 # EXERCISE 9.2.
 # Write a function called has_no_e that returns True if the given word doesn’t have the letter “e” in it.
+
 def has_no_e(word):
     for i in word:
         if i == 'e':
@@ -19,6 +21,7 @@ print(has_no_e('aespa'))
 
 # Write a program that reads words.txt and prints only the words that have no “e”
 # Compute the percentage of words in the list that have no “e”.
+
 fin = open("words.txt")
 total = 0
 no_e_count = 0
@@ -37,6 +40,7 @@ print(no_e_pct, 'percentage of words in the list that have no “e”.')
 # EXERCISE 9.3.
 # Write a function named avoids that takes a word and a string of forbidden letters,
 # and that returns True if the word doesn’t use any of the forbidden letters.
+
 def avoid(word, forb):
     for i in word:
         if i == forb:
@@ -60,6 +64,7 @@ print(count)
 # EXERCISE 9.4. 
 # Write a function named uses_only that takes a word and a string of letters, 
 # and that returns True if the word contains only letters in the list
+
 def uses_only(word, available):
     for letter in word:
         if letter not in available:
@@ -67,8 +72,9 @@ def uses_only(word, available):
         return True
     
 # EXERCISE 9.5.
-# Write a function named uses_only that takes a word and a string of letters, and
-# that returns True if the word contains only letters in the list
+# Write a function named uses_only that takes a word and a string of letters, 
+# and that returns True if the word contains only letters in the list
+
 def uses_all(word, required):
     for letter in required:
         if letter not in word:
@@ -76,6 +82,7 @@ def uses_all(word, required):
         return True
 
 # How many words are there that use all the vowels aeiou? How about aeiouy?
+
 fin = open('words.txt')
 required = 'aeiou'
 count = 0
@@ -93,6 +100,7 @@ print(count)
 # How many abecedarian words are there?
 
 # Approach 1. Use for loop
+
 def is_abecedarian(word):
     previous = word[0]
         for c in word:
@@ -102,6 +110,7 @@ def is_abecedarian(word):
         return True
     
 # Approach 2. Use recursion
+
 def is_abecedarian(word):
     if len(word) <= 1:
         return True
@@ -110,6 +119,7 @@ def is_abecedarian(word):
     return is_abecedarian(word[1:])
 
 # Approach 3. Use while loop
+
 def is_abecedarian(word):
     i = 0
     while i < len(word)-1:
@@ -120,6 +130,7 @@ def is_abecedarian(word):
 
 # EXERCISE 9.7.
 # This question is based on a Puzzler (http://www.cartalk.com/content/puzzlers):
+
 def check_triple_pairs(word):
     i = 0
     count = 0
@@ -145,6 +156,7 @@ find_triple_pairs()
 
 
 # EXERCISE 9.8.
+
 def palindrome():
     for i in range(100000, 1000000):
         if str(i)[2:6] == str(i)[6:1:-1]:                  # last 4 digits
