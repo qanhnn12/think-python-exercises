5d4741f2cb31bedbb9c5e64691d31483522cb544
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2021-08-22 20:24:49 +0700

    Update chapter9.py

--- chapter9.py
+++ chapter9.py
@@ -1,11 +1,14 @@
 # EXERCISE 9.1. 
 # Write a program that reads words.txt, prints words with more than 20 Characters (not counting whitespace).
 
-fin = open('words.txt')
-for line in fin:
-    word = line.strip()
-    if len(word) > 20:
-        print(word)
+def read_word():
+    fin = open('words.txt')
+    for line in fin:
+        word = line.strip()
+        if len(word) > 20:
+            print(word)
+
+read_word()
 
         
 # EXERCISE 9.2.
@@ -22,19 +25,22 @@ print(has_no_e('aespa'))
 # Write a program that reads words.txt and prints only the words that have no “e”
 # Compute the percentage of words in the list that have no “e”.
 
-fin = open("words.txt")
-total = 0
-no_e_count = 0
+def pct_no_e():
+    fin = open("words.txt")
+    total = 0
+    no_e_count = 0
 
-for line in fin:
-    word = line.strip()
-    if 'e' not in word:                         # or use the has_no_e function above: if has_no_e(word)
-        no_e_count += 1
-        print(word)
-    total += 1
+    for line in fin:
+        word = line.strip()
+        if 'e' not in word:                         # or use the has_no_e function above: if has_no_e(word)
+            no_e_count += 1
+            print(word)
+        total += 1
 
-no_e_pct = no_e_count / total * 100
-print(no_e_pct, 'percentage of words in the list that have no “e”.')
+    no_e_pct = no_e_count / total * 100
+    print(no_e_pct, 'percentage of words in the list that have no “e”.')
+
+pct_no_e()
 
 
 # EXERCISE 9.3.
