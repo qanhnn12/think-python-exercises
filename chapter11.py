38338b3541046cfa63763b7c51686541fd7974e7
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2021-08-25 11:14:14 +0700

    Update chapter11.py

--- chapter11.py
+++ chapter11.py
@@ -57,6 +57,7 @@ print(elapsed_time, 'seconds')
 # Use bi_insect() function: 0.05539584159851074 seconds
 
 
+
 # EXERCISE 11.2.
 
 def invert_dict(d):
@@ -73,4 +74,47 @@ d = {'a': 0, 'b': 1, 'c': 1, 'd': 2}
 
 print(invert_dict(d))
 
+
+
+
 # EXERCISE 11.3.
+cache = {}
+
+
+def ack(m, n):
+    if m == 0:
+        return n+1
+    elif m > 0 and n == 0:
+        return ack(m-1, 1)
+
+    if (m, n) in cache:
+        return cache[m, n]
+    else:
+        cache[m, n] = ack(m-1, ack(m, n-1))
+        return cache[m, n]
+
+
+print(ack(3, 4))
+print(ack(3, 6))
+
+
+
+# EXERCISE 11.3.
+
+d = {}
+
+def has_duplicates(t):
+    for x in t:
+        
+        # Python dictionaries don't support duplicate keys
+        if x in d:
+            return True
+        # Append the value into d (we can remove this step if we don't care about d)
+        d[x] = True
+    return False
+
+t = [1,2,3,4]
+print(has_duplicates(t))
+t.append(1)
+print(has_duplicates(t))
+print(d)
