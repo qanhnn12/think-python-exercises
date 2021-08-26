acbc13e3fa6d5d61d0661d2f3dfb16e6386d79c0
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2021-08-26 16:10:06 +0700

    Update chapter8.py

--- chapter8.py
+++ chapter8.py
@@ -4,7 +4,7 @@ print(word.count('a'))
 
 # EXERCISE 8.3
 def is_palindrome(word):
-    return len(word) > 1 and word == word[::-1]:
+    return word == word[::-1]
         
 print(is_palindrome('iloveu'))
 print(is_palindrome('noon'))
