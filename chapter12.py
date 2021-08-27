c8f6dd6b4e32d620932108ac47732ef8720c0b19
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2021-08-27 09:53:57 +0700

    Update chapter12.py

--- chapter12.py
+++ chapter12.py
@@ -1,6 +1,11 @@
+# EXERCISE 12.1.
+
 def histogram(s):
     # create a dictionary in which each item is a character-frequency pair
     d = dict()
+    
+    # set value = 1 by default
+    # if c has already existed, increment it by 1
     for c in s:
         d[c] = 1 + d.get(c, 0)
     return d
@@ -26,4 +31,56 @@ def most_frequent(s):
 
     return result
 
+
 print(most_frequent('pneumonoultramicroscopicsilicovolcanoconiosis'))
+
+
+
+# EXERCISE 12.2.
+'''
+1. Write a program that reads a word list from a file. 
+Prints all the sets of words that are anagrams.
+'''
+
+# return a string that contains all letter in ascending order
+def ordered_string(s):
+    t = list(s)
+    t.sort()
+    t = ''.join(t)
+    return t
+
+
+# create a dictionary that maps from a collection of letters 
+# to a list of words that can be spelled with those letters
+def all_anagrams(filename):    
+    d = dict()
+    for line in open(filename):
+        word = line.strip().lower()
+        
+        # t is a string that contains all letters
+        # from which can form a word in the list
+        t = ordered_string(word)
+        
+        if t not in d:
+            d[t] = [word]
+        else:
+            d[t].append(word)
+            
+
+def anagram_sets(d):
+    for v in d.values():
+        
+        # if each key has only 1 value, it doesn't have any anagram in the word list
+        if len(v) > 1:
+            print(v)
+  
+
+d = all_anagrams()
+anagram_sets(d)
+     
+
+
+'''
+2. Modify the previous program so that it prints the longest list of anagrams first, 
+followed by the second longest, and so on.
+'''
