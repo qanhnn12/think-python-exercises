4a4ceb0817055291bf60f91305c34cdb8df17714
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2021-08-30 17:42:15 +0700

    Update chapter13.py

--- chapter13.py
+++ chapter13.py
@@ -10,7 +10,7 @@ def read_file(filename):
     d = {}
     data = open(filename)
     
-    # for each line in the data, replace the hyphen by the empty string
+    # for each line in the data, replace the hyphens by spaces
     for line in data:
         line = line.replace('-', ' ')
 
@@ -18,7 +18,8 @@ def read_file(filename):
         for word in line.split():
             word = word.strip(string.punctuation + string.whitespace)
             word = word.lower()
-
+            
+            # increment the value by 1 if the word(key) hasn't existed in the dictionary
             d[word] = d.get(word, 0) + 1
     return d
   
@@ -45,7 +46,7 @@ def different_word(d):
     return len(d)
 
 
-d = read_file('filename')
+d = read_file(filename)
 print('Total number of words:', total_words(d))
 print('Number of different words:', different_word(d))
 
@@ -102,11 +103,12 @@ def subtract(d1, d2):
             res[key] = None
     return res
 
-d1 = read_file('28500-8.txt')
-d2 = read_file('words.txt')
+d1 = read_file(filename1)
+d2 = read_file(filename2)
 
 diff = subtract(d1,d2)
 print("Words in the books that are not in the word list are:")
+
 for word in diff:
     print(word, end=' ')
 
@@ -122,3 +124,17 @@ to frequency. For example, for this histogram:
 {'a': 2, 'b': 1}
 your function should return 'a' with probability 2/3 and 'b' with probability 1/3.
 '''
+
+def choose_from_hist(d):
+    t = []
+    for word, freq in d.items():
+        t.extend([word] * freq)
+    return random.choice(t)
+
+
+d = read_file(filename)
+
+print("\n\nHere are some random words from the book:")
+
+for i in range(100):
+    print(choose_from_hist(d), end=' ')
