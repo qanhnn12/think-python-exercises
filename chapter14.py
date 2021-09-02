0a5aef7a19b1be42e0093215464d5c7b6ad3c378
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2021-09-02 20:50:42 +0700

    Update chapter14.py

--- chapter14.py
+++ chapter14.py
@@ -19,3 +19,37 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
+# EXERCISE 14.2.
+
+from anagram_sets import all_anagrams
+
+# store the anagram dictionary in a “shelf”
+def store_anagrams(filename):
+    shelf = dict()
+    d = all_anagrams(filename)
+    for word, word_list in d.items():
+        if len(word_list) > 1:
+            shelf[word] = word_list
+    return shelf
+
+print(store_anagrams('words.txt'))
+
+
+# look up a word and return a list of its anagrams
+def read_anagrams(filename):
+    t = []
+    word = input("Enter a word you want to look up: ")
+    anagrams = store_anagrams('words.txt')
+    for k, v in anagrams.items():
+        if k == word:
+            for w in v:
+                t.append(w)
+        else:
+            return 'That word is not in the list.'
+    return t
+
+print(read_anagrams('words.txt'))
+
